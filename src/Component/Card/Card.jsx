60917bbdf0f068246b161c32eb5fbee8e15148fc
Parbez Mosharof <136151341+parbez01@users.noreply.github.com>
2023-09-13 18:54:36 +0600

    update

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -7,12 +7,12 @@ const Card = ({ singleCountryData, handleCountry }) => {
     return (
         <div className="">
             <div className="my-8 card ">
-                <h2>Country Name: {name.common} </h2>
+                <h2 className='text-center font-semibold mb-8'>Country Name: {name.common} </h2>
                 <img className="w-[500px] h-[300px]  " src={flags.png} alt="" />
-                <p>Population: {population} </p>
-                <p>Capital: {capital} </p>
-                <p>Languages: {languages?.fra} </p>
-                <p>CIOC: {cioc}</p>
+                <p className='text-center font-semibold mt-6'>Population: {population} </p>
+                <p className='text-center font-semibold'>Capital: {capital} </p>
+                <p className='text-center font-semibold'>Languages: {languages?.fra} </p>
+                <p className='text-center font-semibold'>CIOC: {cioc}</p>
                <div className='flex justify-center'>
                <button onClick={() => handleCountry(name.common)} className="bg-pink-600 p-3 rounded-lg mt-4 text-white mb-10">Mark As</button>
                </div>
